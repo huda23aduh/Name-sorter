4789762ba3180c7578c14f94286c7e00ea41c459
huda23aduh <huda23aduh@gmail.com>
2019-09-13 08:34:29 +0700

    make fileCheckingFunc

--- Name-sorter/Program.cs
+++ Name-sorter/Program.cs
@@ -8,50 +8,64 @@ namespace Name_sorter
 {
     class Program
     {
+        static string the_dir = AppDomain.CurrentDomain.BaseDirectory; //working directory
+        static string inputfileextension = ".txt"; //input file extension
+
         static void Main(string[] args)
         {
-            mainProcessFunc("unsorted-names-list");
+            mainProcessFunc("unsorted-names-listaa");
         }
 
-        static void mainProcessFunc(string filename)
+        private static void mainProcessFunc(string filename)
         {
             Stopwatch stopwatch = Stopwatch.StartNew(); //creates and start the instance of Stopwatch
+            
+            var aa = File.Exists(the_dir + filename + inputfileextension);
 
-            string the_dir = AppDomain.CurrentDomain.BaseDirectory;
-
-            string inputfilename = "unsorted-names-list"; //input file name
-            string inputfileextension = ".txt"; //input file extension
-
-            //main process of name-sort
-            var lines = File.ReadLines(the_dir + inputfilename + inputfileextension)
-            .Select(item => {
-                int lastSpace = item.LastIndexOf(' ');
-                return new
-                {
-                    Name = item.Substring(0, lastSpace).Trim(),
-                    Firstname = item.Substring(lastSpace, item.Length - lastSpace).Trim()
-                };
-            })
-            .OrderBy(x => x.Firstname)
-            .ThenBy(x => x.Name)
-            .ToList();
-
-            StringBuilder outputLines = new StringBuilder(); //declare stringbuilder
-            //iterate list and append to string builder variable
-            foreach (var s in lines)
+            if(!aa)
             {
-                outputLines.AppendLine(s.Name + " " + s.Firstname);
+                Console.WriteLine("File input not exists in working directory.");
+                Console.WriteLine("Please check again.");
+                Console.ReadLine();
             }
+            else
+            {
+                //main process of name-sort
+                var lines = File.ReadLines(the_dir + filename + inputfileextension)
+                .Select(item => {
+                    int lastSpace = item.LastIndexOf(' ');
+                    return new
+                    {
+                        Name = item.Substring(0, lastSpace).Trim(),
+                        Firstname = item.Substring(lastSpace, item.Length - lastSpace).Trim()
+                    };
+                })
+                .OrderBy(x => x.Firstname)
+                .ThenBy(x => x.Name)
+                .ToList();
 
-            Console.WriteLine(outputLines); //print to screen
+                StringBuilder outputLines = new StringBuilder(); //declare stringbuilder
+                                                                 //iterate list and append to string builder variable
+                foreach (var s in lines)
+                {
+                    outputLines.AppendLine(s.Name + " " + s.Firstname);
+                }
 
-            File.WriteAllText(the_dir + "sorted-names-list.txt", outputLines.ToString()); //write outputfile
+                Console.WriteLine(outputLines); //print to screen
 
-            //code for count execution time
-            System.Threading.Thread.Sleep(500);
-            stopwatch.Stop();
-            Console.WriteLine("execution time : " + stopwatch.ElapsedMilliseconds + " ms");
-            Console.ReadLine();
+                File.WriteAllText(the_dir + "sorted-names-list.txt", outputLines.ToString()); //write outputfile
+
+                //code for count execution time
+                System.Threading.Thread.Sleep(500);
+                stopwatch.Stop();
+                Console.WriteLine("execution time : " + stopwatch.ElapsedMilliseconds + " ms");
+                Console.ReadLine();
+            }
+        }
+
+        private bool fileCheckingFunc(string filename)
+        {
+            return File.Exists(the_dir + filename + inputfileextension);
         }
     }
 }
