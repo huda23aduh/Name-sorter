b0b9a5326b14125a59cf86e5011b67b7a13a3aa6
huda23aduh <huda23aduh@gmail.com>
2019-09-13 08:12:29 +0700

    make mainProcessFunc

--- Name-sorter/Program.cs
+++ Name-sorter/Program.cs
@@ -10,6 +10,11 @@ namespace Name_sorter
     {
         static void Main(string[] args)
         {
+            mainProcessFunc("unsorted-names-list");
+        }
+
+        static void mainProcessFunc(string filename)
+        {
             Stopwatch stopwatch = Stopwatch.StartNew(); //creates and start the instance of Stopwatch
 
             string the_dir = AppDomain.CurrentDomain.BaseDirectory;
@@ -30,7 +35,7 @@ namespace Name_sorter
             .OrderBy(x => x.Firstname)
             .ThenBy(x => x.Name)
             .ToList();
-            
+
             StringBuilder outputLines = new StringBuilder(); //declare stringbuilder
             //iterate list and append to string builder variable
             foreach (var s in lines)
@@ -39,13 +44,13 @@ namespace Name_sorter
             }
 
             Console.WriteLine(outputLines); //print to screen
-            
+
             File.WriteAllText(the_dir + "sorted-names-list.txt", outputLines.ToString()); //write outputfile
 
             //code for count execution time
             System.Threading.Thread.Sleep(500);
             stopwatch.Stop();
-            Console.WriteLine("execution time : "+ stopwatch.ElapsedMilliseconds + " ms");
+            Console.WriteLine("execution time : " + stopwatch.ElapsedMilliseconds + " ms");
             Console.ReadLine();
         }
     }
